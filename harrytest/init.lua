27f5df5b5c126d12556ae98674df3f8384845dea
Misael Roman <hyp3rlink@gmail.com>
2012-09-27 16:58:37 -0500

    wand added
    added a wand node (texture needs work) still working on ideas

---
+++ harrytest/init.lua
@@ -0,0 +1,19 @@
+-- Items
+
+
+----1. Wand
+
+minetest.register_tool("harrytest:wand", {
+	description = "Magic Wand",
+	inventory_image = "wand.png",
+	tool_capabilities = {
+		full_punch_interval = 1.0,
+		max_drop_level=1,
+		groupcaps={
+			fleshy={times={[1]=2.00, [2]=0.80, [3]=0.40}, uses=10, maxlevel=2},
+			snappy={times={[2]=0.70, [3]=0.30}, uses=40, maxlevel=1},
+			choppy={times={[3]=0.70}, uses=40, maxlevel=0}
+		}
+	}
+})
+
